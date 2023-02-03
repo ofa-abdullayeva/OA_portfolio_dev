8367482af6f67298d01600fe88282ffa2896d694
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2023-02-03 19:41:46 +0400

    Color change

--- src/styles/GlobalSyle.js
+++ src/styles/GlobalSyle.js
@@ -20,13 +20,12 @@ const GlobalStyle = createGlobalStyle`
       --scrollbar-track-color: #383838;
   } */
   .light-theme{
-    --primary-color: #283739;
-    /* --primary-color-light: #057FFF; */
-    --primary-color-light: #004445;
+    --primary-color: #007bff;
+    --primary-color-light: #057FFF;
     --secondary-color: #ff7675;
     --background-dark-color: #F1F1F1;
     --background-dark-grey: #e4e4e4;
-    --border-color: #004445;
+    --border-color: #cbced8;
     --background-light-color: #F1F1F1;
     --background-light-color-2: rgba(3,127,255,.3);
     --white-color: #151515;
@@ -39,23 +38,19 @@ const GlobalStyle = createGlobalStyle`
     --scrollbar-track-color: #383838;
 }
 .dark-theme{
-    --primary-color: #ed6363;
-    /* --primary-color-light: #057FFF; */
-    --primary-color-light: #004445;
+  --primary-color: #007bff;
+    --primary-color-light: #057FFF;
     --secondary-color: #6c757d;
-    /* --background-dark-color: #10121A; */
-    --background-dark-color: #004445;
+    --background-dark-color: #10121A;
     --background-dark-grey: #191D2B;
-    /* --border-color: #2e344e; */
-    --border-color: #F1F1F1;
+    --border-color: #2e344e;
     --background-light-color: #F1F1F1;
     --background-light-color-2: rgba(3,127,255,.3);
     --white-color: #FFF;
     --font-light-color: #a4acc4;
     --font-dark-color: #313131;
     --font-dark-color-2: #151515;
-    /* --sidebar-dark-color: #191D2B; */
-    --sidebar-dark-color: #2c786c;
+    --sidebar-dark-color: #191D2B;
     --scrollbar-bg-color: #383838;
     --scrollbar-thump-color: #6b6b6b;
     --scrollbar-track-color: #383838;
